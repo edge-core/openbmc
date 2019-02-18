c940454fefd6fe9a60026618a3bbc9259ea4491d
JeremyChen1004 <jeremy_chen@accton.com>
2019-02-18 17:54:06 +0800

    Enhance cpld_rev.sh to support upper(lower) sys(fan) cpld version display

--- meta-bf/meta-mavericks/recipes-utils/openbmc-utils/files/cpld_rev.sh
+++ meta-bf/meta-mavericks/recipes-utils/openbmc-utils/files/cpld_rev.sh
@@ -17,9 +17,78 @@
 # 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA
 #
+. /usr/local/bin/openbmc-utils.sh
 
-rev=$(cat /sys/class/i2c-adapter/i2c-12/12-0031/cpld_rev)
-sub_rev=$(cat /sys/class/i2c-adapter/i2c-12/12-0031/cpld_sub_rev)
+board_type=$(wedge_board_type)
+board_subtype=$(wedge_board_subtype)
 
-echo $(($rev)).$(($sub_rev))
+if [ "$board_subtype" == "Mavericks" ]; then
+    FAN_DIR=/sys/class/i2c-adapter/i2c-8/8-0033
+    FAN_DIR_UPPER=/sys/class/i2c-adapter/i2c-9/9-0033
+    SYS_DIR=/sys/class/i2c-adapter/i2c-12/12-0031
+    SYS_DIR_UPPER=/sys/class/i2c-adapter/i2c-12/12-0030
+elif [ "$board_subtype" == "Montara" ]; then
+    FAN_DIR=/sys/class/i2c-adapter/i2c-8/8-0033
+    SYS_DIR=/sys/class/i2c-adapter/i2c-12/12-0031
+elif [ "$board_subtype" == "Newport" ]; then
+    FAN_DIR=/sys/class/i2c-adapter/i2c-8/8-0066
+    SYS_DIR=/sys/class/i2c-adapter/i2c-12/12-0031
+fi
+
+usage() {
+    echo "Usage: ${0} <target board (upper lower)> <target cpld (sys fan)>" >&2
+}
+
+# Check the number of arguments provided
+if [ $# -ne 2 ]; then
+    usage
+    exit 1
+fi
+
+if [ $1 != "upper" ] && [ $1 != "lower" ]; then
+    usage
+    exit 1
+fi
+if [ $2 != "sys" ] && [ $2 != "fan" ]; then
+    usage
+    exit 1
+fi
+
+# 2U: Mavericks, 1U: Montara, Newport
+if [ $1 == "upper" ] && [ "$board_subtype" != "Mavericks" ]; then
+    echo "upper board does not exist"
+    exit 1
+fi
+
+# Check the file name and upgrade accordingly
+if [ $1 == "upper" ]; then
+    if [ $2 == "sys" ]; then
+        rev="${SYS_DIR_UPPER}/cpld_rev"
+        sub_rev="${SYS_DIR_UPPER}/cpld_sub_rev"
+    else
+        rev="${FAN_DIR_UPPER}/cpld_rev"
+        sub_rev="${FAN_DIR_UPPER}/cpld_sub_rev"
+    fi
+elif [ $1 == "lower" ]; then
+    if [ $2 == "sys" ]; then
+        rev="${SYS_DIR}/cpld_rev"
+        sub_rev="${SYS_DIR}/cpld_sub_rev"
+    else
+        rev="${FAN_DIR}/cpld_rev"
+        sub_rev="${FAN_DIR}/cpld_sub_rev"
+    fi
+else
+  usage
+  exit 1
+fi
+
+# According to fancpld.c definition
+if [ "$board_type" == "NEWPORT" ] && [ $2 == "fan" ]; then
+    rev_hex2dec=$(cat $rev)
+    echo $(($rev_hex2dec))
+else
+    rev_hex2dec=$(cat $rev)
+    sub_rev_hex2dec=$(cat $sub_rev)
+    echo $(($rev_hex2dec)).$(($sub_rev_hex2dec))
+fi
 
