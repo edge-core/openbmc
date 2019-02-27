0f1153d164f05ea6648ea41c43d36959e511f7c3
JeremyChen1004 <jeremy_chen@accton.com>
2019-02-27 15:24:58 +0800

    Remove 'meta-jbay' due to one binary

--- meta-bf/meta-jbay/recipes-bsp/u-boot/u-boot_2013.07.bbappend
+++
@@ -1,4 +0,0 @@
-FILESEXTRAPATHS_prepend := "${THISDIR}/files:"
-
-SRC_URI += "file://patch-2017.02/0001-u-boot-openbmc-jbay_base.patch \
-          "
--- meta-bf/meta-jbay/recipes-utils/openbmc-utils/files/enable_watchdog_ext_signal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-. /usr/local/bin/openbmc-utils.sh
-
-# select WDTRST
-devmem_clear_bit $(scu_addr 2C) 1
-devmem_set_bit $(scu_addr 84) 4
