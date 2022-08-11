a9e9a28597102361f893304aad1401d0f782be9e
wanju_sun <wanju_sun@accton.com.cn>
2022-08-11 20:02:35 +0800

    get bmc i2c speed
    [Root Cause]
    NA
    
    [Solution]
    NA
    
    [Affected Functions]
    NA
    
    [Verification]
    OK.

--- meta-bf/meta-mavericks/recipes-utils/openbmc-utils/files/diag_i2c.sh
+++ meta-bf/meta-mavericks/recipes-utils/openbmc-utils/files/diag_i2c.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-APB_clock=24750
+#SCU70[9:8]= 01 360MHz    SCU08[25:23]= 011  PCLK = H-PLL/8
+APB_clock=45000
 
 val=`busybox devmem 0x1e78a044 32`
 val=$((${val} & 0xfffff))
@@ -9,6 +10,6 @@ CK_low=$(((($val & 0xf000)>>12)+1))
 CK_high=$(((($val & 0xf0000)>>16)+1))
 
 Freq_SCL=$(($APB_clock/$(($Basecyc * ($CK_low+$CK_high)))))
-echo "I2C: Freq_SCL $Freq_SCL kHz"
+echo "BMC i2c speed: $Freq_SCL kHz"
 
 
