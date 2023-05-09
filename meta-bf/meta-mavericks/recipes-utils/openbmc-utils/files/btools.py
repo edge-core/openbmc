293bdf800327d499650f5f92191bb5b8631f09dd
wanju_sun <wanju_sun@accton.com.cn>
2023-05-09 10:08:43 +0800

    解决65x 设备sensors档位错乱问题。
    [Root Cause]
    NA
    
    [Solution]
    NA
    
    [Affected Functions]
    NA
    
    [Verification]
    OK.

--- meta-bf/meta-mavericks/recipes-utils/openbmc-utils/files/btools.py
+++ meta-bf/meta-mavericks/recipes-utils/openbmc-utils/files/btools.py
@@ -3973,7 +3973,7 @@ def error_usage():
 def main(argv):
     os.system("touch /tmp/btools_lock")
     try:
-        opts, args = getopt.getopt(argv[1:], "hP:U:I:T:", ["help", "PSU=", "UCD=", "IR=", "TMP="])
+        opts, args = getopt.getopt(argv[1:], "hP:U:I:T:F", ["help", "PSU=", "UCD=", "IR=", "TMP=","PLT="])
 
         # No standard identifier.print the usage
         if len(opts) == 0:
@@ -4000,6 +4000,9 @@ def main(argv):
             ir(argv)
         elif opt in ("-T", "--TMP"):
             tmp(argv)
+        elif opt in ("-F", "--PLT"):
+            platform = get_project()
+            print platform
         else:
             error_usage()
 
